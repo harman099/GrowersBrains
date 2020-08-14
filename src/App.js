a0bd20222d5c5dd580503628656034695518ea36
Hridayesh Sharma <dev.hridayesh@gmail.com>
2020-08-14 21:43:08 +0530

    set jsx single quotes to true in prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-  Route,
-  Switch,
-  Redirect,
-  BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+	BrowserRouter as Router,
 } from 'react-router-dom';
 
 import Landing from './screens/Landing';
@@ -18,28 +18,28 @@ import GreenHouse from './screens/GreenHouse';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const privateRouteProps = [
-  { path: '/dashboard', component: Dashboard, exact: true },
-  { path: '/products', component: Products },
-  { path: '/dailygrow', component: DailyGrow },
-  { path: '/consulting', component: Consulting },
-  { path: '/greenhouse', component: GreenHouse },
+	{ path: '/dashboard', component: Dashboard, exact: true },
+	{ path: '/products', component: Products },
+	{ path: '/dailygrow', component: DailyGrow },
+	{ path: '/consulting', component: Consulting },
+	{ path: '/greenhouse', component: GreenHouse },
 ];
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        {privateRouteProps.map((props) => (
-          <ProtectedRoute {...props} key={props.path} />
-        ))}
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/" component={Landing} />
-        {/* If there are no route matches... */}
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Switch>
+				{privateRouteProps.map((props) => (
+					<ProtectedRoute {...props} key={props.path} />
+				))}
+				<Route exact path="/signup" component={SignUp} />
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/" component={Landing} />
+				{/* If there are no route matches... */}
+				<Redirect to="/" />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
